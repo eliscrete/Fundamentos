34f1b650f7bec3d16a1a3a0428c399ff117d0491
Viviane Medeiros <65632616+eliscrete@users.noreply.github.com>
2021-07-05 23:07:40 -0300

    Continuação do curso de introdução ao Java- Fundamentos. Onde foi acrescentado, a taboada/ divisão por zero/ contagem regressiva.

---
+++ src/main/java/fundamentos/Medidas.java
@@ -0,0 +1,143 @@
+// 1 - Pacote
+        package fundamentos;
+
+// 2 - Bibliotecas
+        import java.util.Scanner;
+
+// 3 - Classe
+
+public class Medidas {
+    // 3.1 - Atributos
+
+    static Scanner entrada = new Scanner(System.in);
+
+    // 3.2 - Métodos e Funções
+
+    public static void main(String[] args){
+        String opcao = "1";
+        int area = 0; // receber o resultado dos cálculos de áreas
+
+        while(!opcao.toUpperCase().equals("S")) {
+
+            System.out.println("Escolha o Calculo Desejado");
+            System.out.println("(1) - Area do Quadrado");
+            System.out.println("(2) - Area do Retangulo");
+            System.out.println("(3) - Area do Triangulo");
+            System.out.println("(4) - Area do Circulo");
+            System.out.println("(5) - Tabuada");
+            System.out.println("(6) - Fibonacci");
+            System.out.println("(7) - Contagem Regressiva");
+            System.out.println("(8) - Divisão por Zero");
+            System.out.println("(S) - Sair");
+            opcao = entrada.nextLine(); // leitura da opção
+            switch (opcao) {
+                case "1":
+                    area = calcularAreaDoQuadrado();
+                    break;
+                case "2":
+                    // Todo: calcular área do retangulo
+                    break;
+                case "5":
+                    tabuada();
+                    break;
+                case "6":
+                    fibonacci();
+                    break;
+                case "7":
+                    contagemRegressiva();
+                    break;
+                case "8":
+                    divisaoPorZero();
+                    break;
+                case "s":
+                case "S":
+                    System.out.println("Agradecemos pela preferencia! Fui!!");
+                    break;
+                default:
+                    System.out.println("Opcao Invalida: " + opcao);
+            }
+            if (area > 0) {
+                System.out.println("A área é de " + area + "m²");
+            }
+        }
+
+    }
+
+    public static int calcularAreaDoQuadrado(){
+        int lado; // tamanho do lado do quadrado
+        System.out.println("Digite o tamanho do lado: ");
+        lado = entrada.nextInt(); // leitura do tamanho do lado
+
+        // Desenhar o quadrado
+        for (int linha = 1; linha <= lado; linha+=2) {
+            for (int coluna = 1; coluna <= lado; coluna++) {
+                System.out.print("#");
+            }
+        }
+        System.out.println(""); // pular de linha
+        return lado * lado; // retorna a área do quadrado
+    }
+    public static void tabuada(){
+        System.out.println("Você quer calcular a tabuada de qual numero? ");
+        byte numero = entrada.nextByte();
+        for (byte i=1;i<=10;i++){
+            System.out.println (numero * i + " ");
+        }
+    }
+    public static void fibonacci(){
+        System.out.println("Quantos números deseja calcular na sequencia? ");
+        byte numero = entrada.nextByte();
+        switch(numero){
+            case 0:
+                System.out.println("A sequencia está vazia.");
+                break;
+            case 1:
+                System.out.println("Sequencia de Fibonacci: 1");
+                break;
+            default:
+                int num1 = 0;
+                int num2 = 1;
+                System.out.println("Sequencia de Fibonacci: 1 ");
+                for(byte i = 2; i <= numero; i++ ){
+                    int fib = num1 + num2;
+                    System.out.println(fib + " ");
+                    num1 = num2;
+                    num2 = fib;
+                }
+        }
+    }
+    public static void contagemRegressiva(){
+        System.out.println("Começar a contagem a partir de qual numero? ");
+        int numero = entrada.nextInt();
+        System.out.println("Diminuindo de quantos em quantos numeros por vez? ");
+        int decrescimo = entrada.nextInt();
+        for(int i = numero; i >= 1; i-= decrescimo){
+            System.out.println(i);
+        }
+    }
+
+    public static void divisaoPorZero(){
+
+        try {
+        System.out.println("Qual é o dividendo? (numero a ser divido) ");
+        int dividendo = entrada.nextInt();
+        System.out.println("Qual é o divisor? (numero que divide o dividendo) ");
+        int divisor = entrada.nextInt();
+
+
+            System.out.println(" O resultado é:" + dividendo / divisor);
+
+        }
+        catch(Exception e){
+
+            System.out.println( e.getMessage());// exibe o erro pro usuario
+
+            }
+
+        finally {
+            System.out.println("Por hoje é só pessoal!!");
+        }
+
+    }
+
+}
